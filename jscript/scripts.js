6cb2bff9d0c179481e9200537752ce36463a2c17
anmol224 <anmolj124@gmail.com>
2021-03-28 12:42:42 +0530

    Updates

---
+++ jscript/scripts.js
@@ -0,0 +1,11 @@
+const toggle=document.querySelector('.toggle')
+const navigation=document.querySelector('.navigation')
+
+setTimeout(() => {
+   
+        toggle.classList.add('active')
+        navigation.classList.add('active')
+  
+    
+}, 2000);
+
